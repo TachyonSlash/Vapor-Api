d8eca8a17fb4f8df2156cf50a2f58904ef1c877d
GerardoAV1 <gerardo_amezquita@outlook.com>
2025-06-06 15:45:06 -0600

    Agregue GamesGenresController

---
+++ Backlog-api/Sources/BacklogApi/Controllers/GamesGenresController.swift
@@ -0,0 +1,36 @@
+import Vapor 
+import Fluent
+
+struct GamesGenresController : RouteCollection {
+    func boot(routes: any RoutesBuilder) throws {
+        let gamesGenres = routes.grouped("gamesgenres")
+        
+        gamesGenres.get(use: self.getGamesGenres)
+        gamesGenres.post(use: self.create)
+    }
+
+    func getGamesGenres(req: Request) async throws -> [GameGenreDTO] {
+        var gameGenreMock: [GameGenreDTO] = []
+        let gameGenresDB = try await GameGenre.query(on: req.db).all()
+        gameGenresDB.forEach { gameGenre in
+            let dto = GameGenreDTO(
+                id: gameGenre.id,
+                gameId: gameGenre.gameId,
+                genreId: gameGenre.genreId
+            )
+            gameGenreMock.append(dto)
+        }
+        return gameGenreMock
+    }
+
+    func create(req: Request) async throws -> GameGenreDTO {
+        let dto = try req.content.decode(GameGenreDTO.self)
+        let gameGenre = GameGenre(
+            id: dto.id,
+            gameId: dto.gameId,
+            genreId: dto.genreId
+        )
+        try await gameGenre.save(on: req.db)
+        return dto
+    }
+}
